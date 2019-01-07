03e5ba5e18a3b4d108d1a63e479dec496abdb71c
Marek Fořt <marek.fort@ackee.cz>
2019-01-07 13:40:07 +0100

    Move Secret constant to Constants.

---
+++ Example/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TezosSwift_Example
+//
+//  Created by Marek Fořt on 1/7/19.
+//  Copyright © 2019 Ackee. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    static let defaultNodeURL = URL(string: "https://rpcalpha.tzbeta.net/")!
+}
