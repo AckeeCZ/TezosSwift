7ab13a504bb71cb11e58600df09bb6f559213940
Marek Fořt <marek.fort@ackee.cz>
2019-11-25 08:56:19 +0100

    Make ContractPublisher init public.

--- TezosSwift/Combine/ContractPublisher.swift
+++ TezosSwift/Combine/ContractPublisher.swift
@@ -9,7 +9,7 @@ public struct ContractPublisher<O: Decodable>: Publisher {
     
     private let send: SendMethod<Output>
     
-    init(send: @escaping SendMethod<Output>) {
+    public init(send: @escaping SendMethod<Output>) {
         self.send = send
     }
     
